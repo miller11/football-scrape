cf5fbd686299b01ca1dc85d4bab9728c387335fc
Ross Miller <ross-h-miller@uiowa.edu>
2020-01-18 15:59:39 -0600

    adding multi-threading support

--- football-scrape/code/PlayerGamelogStats.py
+++ football-scrape/code/PlayerGamelogStats.py
@@ -3,6 +3,14 @@ import os
 import gc
 import pandas as pd
 import psutil
+import time
+
+from queue import Queue
+from threading import Thread
+
+from multiprocessing.dummy import Pool as ThreadPool
+
+
 from google.cloud import bigquery  # Imports the Google Cloud client library
 from PlayerGamelogUtil import PlayerGamelogUtil
 
@@ -12,6 +20,24 @@ GAMELOG_STATS_FILENAME = 'player_gamelog_stats.csv'
 def write_to_file(df):
     df.to_csv(os.path.join(files_dir, GAMELOG_STATS_FILENAME), index=False, encoding='utf-8')
     del df
+
+
+def crawl(data):
+    player_link = data[0]
+
+    if '.htm' in player_link:
+        player_gamelog_util = PlayerGamelogUtil(player_link)
+
+        data_frames.append(player_gamelog_util.get_gamelog_stats())
+
+        gc.collect()
+
+        print('Player processed for: {}. CPU%: {}. Memory: {}'.format(player_link, psutil.cpu_percent(),
+                                                                      dict(psutil.virtual_memory()._asdict())))
+
+        time.sleep(1)
+
+    return True
 
 
 # constants
@@ -30,19 +56,12 @@ player_links = bq_query.to_dataframe().values
 
 data_frames = []
 
-# iterate through team links
-for row in player_links:
-    player_link = row[0]
+pool = ThreadPool()
 
-    if '.htm' in player_link:
-        player_gamelog_util = PlayerGamelogUtil(player_link)
+results = pool.map(crawl, player_links)
 
-        data_frames.append(player_gamelog_util.get_gamelog_stats())
-
-        gc.collect()
-
-        print('Player processed for: {}. CPU%: {}. Memory: {}'.format(player_link, psutil.cpu_percent(),
-                                                                          dict(psutil.virtual_memory()._asdict())))
+pool.close()
+pool.join()
 
 write_to_file(pd.concat(data_frames, axis=0, sort=True))
 
